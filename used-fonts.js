f050448e21489a16aafc7c3f3510b1bc20044a56
Lisa Övermyr <lisaovermyr@gmail.com>
2023-07-17 10:09:44 +0200

    Create used-fonts.js

---
+++ used-fonts.js
@@ -0,0 +1,30 @@
+function getUsedFonts() {
+  const els = document.querySelectorAll("*");
+  const fonts = {};
+  [...els].forEach((el) => {
+    const elFamily = getComputedStyle(el)["font-family"];
+    const elWeight = getComputedStyle(el)["font-weight"];
+    fonts[elFamily] = fonts[elFamily] || {};
+    fonts[elFamily][elWeight] = fonts[elFamily]?.[elWeight] + 1 || 1;
+  });
+
+  console.log(fonts);
+}
+
+getUsedFonts();
+
+function getMatchinEls(family, weight) {
+  const els = document.querySelectorAll("*");
+  const matching = [];
+  [...els].forEach((el) => {
+    const elFamily = getComputedStyle(el)["font-family"];
+    const elWeight = getComputedStyle(el)["font-weight"];
+    if (elFamily.includes(family) && elWeight === weight) {
+      matching.push(el);
+    }
+  });
+
+  console.log("matching", matching);
+}
+
+// getMatchinEls('Arial', '400')
